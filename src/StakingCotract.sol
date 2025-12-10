b4ec5e62b9ede8beeaa9f5571f095ea3de4ca0b7
SuhasKanwar <suhas.kanwar@gmail.com>
2025-12-10 22:41:00 +0530

    Added basic staking contract

--- src/StakingCotract.sol
+++ src/StakingCotract.sol
@@ -2,5 +2,23 @@
 pragma solidity ^0.8.13;
 
 contract StakingContract {
+    mapping(address => uint256) stakes;
+    uint256 totalStakes;
+
     constructor() {}
+
+    function stake() public payable {
+        require(msg.value > 0);
+        stakes[msg.sender] += msg.value;
+    }
+
+    function unstake(uint256 _amount) public {
+        require(_amount <= stakes[msg.sender]);
+        payable(msg.sender).transfer(_amount);
+        stakes[msg.sender] -= _amount;
+    }
+
+    function getRewards() public view {}
+
+    function claimRewards() public {}
 }
