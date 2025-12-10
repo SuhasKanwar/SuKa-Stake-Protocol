6c6d6c022d93b443603a29adcb2383dfb2cc1c44
SuhasKanwar <suhas.kanwar@gmail.com>
2025-12-10 23:57:03 +0530

    Added staking logic

--- src/StakingContract.sol
+++ src/StakingContract.sol
@@ -3,17 +3,28 @@ pragma solidity ^0.8.13;
 
 contract StakingContract {
     mapping(address => uint256) stakes;
-    uint256 totalStakes;
+    mapping(address => uint256) unclaimedRewards;
+    mapping(address => uint256) lastUpdateTime;
 
     constructor() {}
 
     function stake() public payable {
         require(msg.value > 0);
+        if (!lastUpdateTime[msg.sender]) {
+            lastUpdateTime[msg.sender] = block.timestamp;
+        } else {
+            unclaimedRewards[msg.sender] += (block.timestamp - lastUpdateTime[msg.sender]) * stakes[msg.sender];
+            lastUpdateTime[msg.sender] = block.timestamp;
+        }
         stakes[msg.sender] += msg.value;
     }
 
     function unstake(uint256 _amount) public {
         require(_amount <= stakes[msg.sender]);
+
+        unclaimedRewards[msg.sender] += (block.timestamp - lastUpdateTime[msg.sender]) * stakes[msg.sender];
+        lastUpdateTime[msg.sender] = block.timestamp;
+
         payable(msg.sender).transfer(_amount);
         stakes[msg.sender] -= _amount;
     }
@@ -22,7 +33,21 @@ contract StakingContract {
         return stakes[_staker];
     }
 
-    function getRewards() public view {}
+    function getRewards(address _address) public view {
+        uint256 currentReward = unclaimedRewards[_address];
+        uint256 lastUpdatedTime = lastUpdateTime[_address];
+        uint256 newReward = (block.timestamp - lastUpdatedTime) * stakes[_address];
+        return currentReward + newReward;
+    }
 
-    function claimRewards() public {}
+    function claimRewards() public {
+        uint256 currentReward = unclaimedRewards[msg.sender];
+        uint256 lastUpdatedTime = lastUpdateTime[msg.sender];
+        uint256 newReward = (block.timestamp - lastUpdatedTime) * stakes[msg.sender];
+
+        // Transfer currentReward + newReward amount of SuKaCoin
+
+        unclaimedRewards[msg.sender] = 0;
+        lastUpdateTime[msg.sender] = block.timestamp;
+    }
 }
