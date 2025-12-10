f1921685eba484856475a70924fe939479a4b639
SuhasKanwar <suhas.kanwar@gmail.com>
2025-12-11 00:36:43 +0530

    Fixed the staking contract

--- src/StakingContract.sol
+++ src/StakingContract.sol
@@ -1,53 +1,88 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
+interface ISuKaCoinContract {
+    function mint(address _account, uint256 _amount) external;
+}
+
 contract StakingContract {
-    mapping(address => uint256) stakes;
-    mapping(address => uint256) unclaimedRewards;
-    mapping(address => uint256) lastUpdateTime;
+    uint256 public totalStake;
+    uint256 public constant REWARD_PER_SEC_PER_ETH = 1;
 
-    constructor() {}
+    struct UserInfo {
+        uint256 stakedAmount;
+        uint256 rewardDebt;
+        uint256 lastUpdateTime;
+    }
 
-    function stake() public payable {
-        require(msg.value > 0);
-        if (!lastUpdateTime[msg.sender]) {
-            lastUpdateTime[msg.sender] = block.timestamp;
-        } else {
-            unclaimedRewards[msg.sender] += (block.timestamp - lastUpdateTime[msg.sender]) * stakes[msg.sender];
-            lastUpdateTime[msg.sender] = block.timestamp;
+    mapping(address => UserInfo) public userInfo;
+    ISuKaCoinContract public suKaCoin;
+
+    constructor(ISuKaCoinContract _suKaCoin) {
+        suKaCoin = _suKaCoin;
+    }
+
+    function _updateRewards(address _staker) internal {
+        UserInfo storage user = userInfo[_staker];
+
+        if (user.lastUpdateTime == 0) {
+            user.lastUpdateTime = block.timestamp;
+            return;
         }
-        stakes[msg.sender] += msg.value;
+
+        uint256 timeDiff = block.timestamp - user.lastUpdateTime;
+
+        if (timeDiff == 0) {
+            return;
+        }
+
+        uint256 additionalReward = (user.stakedAmount * timeDiff * REWARD_PER_SEC_PER_ETH);
+
+        user.rewardDebt += additionalReward;
+        user.lastUpdateTime = block.timestamp;
+    }
+
+    function stake(uint256 _amount) external payable {
+        require(msg.value == _amount, "Sent value must match the stake amount");
+        require(msg.value > 0, "Stake amount must be greater than zero");
+
+        _updateRewards(msg.sender);
+
+        userInfo[msg.sender].stakedAmount += _amount;
+        totalStake += _amount;
     }
 
     function unstake(uint256 _amount) public {
-        require(_amount <= stakes[msg.sender]);
+        require(_amount > 0, "Unstake amount must be greater than zero");
+        UserInfo storage user = userInfo[msg.sender];
+        require(user.stakedAmount >= _amount, "Insufficient staked amount to unstake");
 
-        unclaimedRewards[msg.sender] += (block.timestamp - lastUpdateTime[msg.sender]) * stakes[msg.sender];
-        lastUpdateTime[msg.sender] = block.timestamp;
+        _updateRewards(msg.sender);
+
+        user.stakedAmount -= _amount;
+        totalStake -= _amount;
 
         payable(msg.sender).transfer(_amount);
-        stakes[msg.sender] -= _amount;
     }
 
     function balanceOf(address _staker) public view returns (uint256) {
-        return stakes[_staker];
-    }
-
-    function getRewards(address _address) public view {
-        uint256 currentReward = unclaimedRewards[_address];
-        uint256 lastUpdatedTime = lastUpdateTime[_address];
-        uint256 newReward = (block.timestamp - lastUpdatedTime) * stakes[_address];
-        return currentReward + newReward;
+        return userInfo[_staker].stakedAmount;
     }
 
     function claimRewards() public {
-        uint256 currentReward = unclaimedRewards[msg.sender];
-        uint256 lastUpdatedTime = lastUpdateTime[msg.sender];
-        uint256 newReward = (block.timestamp - lastUpdatedTime) * stakes[msg.sender];
+        _updateRewards(msg.sender);
+        UserInfo storage user = userInfo[msg.sender];
+        suKaCoin.mint(msg.sender, user.rewardDebt);
+        user.rewardDebt = 0;
+    }
 
-        // Transfer currentReward + newReward amount of SuKaCoin
+    function getRewards(address _address) public view {
+        uint256 timeDiff = block.timestamp - userInfo[_address].lastUpdateTime;
 
-        unclaimedRewards[msg.sender] = 0;
-        lastUpdateTime[msg.sender] = block.timestamp;
+        if (timeDiff == 0) {
+            return userInfo[_address].rewardDebt;
+        }
+
+        return (userInfo[_address].stakedAmount * timeDiff * REWARD_PER_SEC_PER_ETH) + userInfo[_address].rewardDebt;
     }
 }
